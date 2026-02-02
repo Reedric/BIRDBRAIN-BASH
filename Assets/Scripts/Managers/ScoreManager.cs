fd02ed981fea7d9771e6d361f83cc07ed461f5d4
Alex Huang <alexhuang0812@gmail.com>
2026-02-01 23:33:25 -0500

    Updated Score Manager
    I added first to win 2 sets (Best of 3 format), first to 15 points, win by 2.

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -5,17 +5,16 @@ public class ScoreManager : MonoBehaviour
 {
     public int side1Score = 0;
     public int side2Score = 0;
+    public int side1SetsWon = 0;
+    public int side2SetsWon = 0;
     public GameManager gameManager;
     private bool leftLastScored;
     private bool inPlay;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // Set the scores to 0, right to serve, and the ball is in play
-        side1Score = 0;
-        side2Score = 0;
-        leftLastScored = false;
-        inPlay = true;
+        // Set the scores to 0, the sets to 0, right to serve, and the ball is in play
+        ResetMatch();
 
         // Make sure game manager is not null in inspector
         if (gameManager == null)
@@ -33,7 +32,7 @@ public class ScoreManager : MonoBehaviour
             side2Score += 1;
             inPlay = false;
             Debug.Log("side 2 scored! points: " + side2Score);
-            CheckWin(true);
+            CheckWinSet(true);
         } 
         // if it touches side 2, then side 1 scores
         else if (collision.gameObject.CompareTag("Side2") && inPlay) 
@@ -41,23 +40,47 @@ public class ScoreManager : MonoBehaviour
             side1Score += 1;
             inPlay = false;
             Debug.Log("side 1 scored! points: " + side1Score);
-            CheckWin(false);
+            CheckWinSet(false);
         }
     }
 
     // After each score, check the win conditions for both sides
-    void CheckWin(bool leftJustScored)
+    void CheckWinSet(bool leftJustScored)
     {
-        if (side1Score >= 3 && side1Score > side2Score + 2)
+        if (side1Score >= 15 && side1Score - side2Score >= 2)
         {
             Debug.Log("side 1 wins! final score: " + side1Score + " to " + side2Score);
+            side1SetsWon++;
+            CheckMatchWin(leftJustScored);
         } 
-        else if (side2Score >= 3 && side2Score > side1Score + 2)
+        else if (side2Score >= 15 && side2Score - side1Score >= 2)
         {
             Debug.Log("side 2 wins! final score: " + side1Score + " to " + side2Score);
+            side2SetsWon++;
+            CheckMatchWin(leftJustScored);
         }
         else
         {
+            StartCoroutine(StartNextPoint(leftJustScored));
+        }
+    }
+    //Checks if the Match is won, Best of 3 format
+    void CheckMatchWin(bool leftJustScored)
+    {
+        if (side1SetsWon == 2)
+        {
+            Debug.Log("Side 1 wins the match!");
+            inPlay = false;
+        }
+        else if (side2SetsWon == 2)
+        {
+            Debug.Log("Side 2 wins the match!");
+            inPlay = false;
+        }
+        else
+        {
+            //Resets the score for next set
+            ResetScore();
             StartCoroutine(StartNextPoint(leftJustScored));
         }
     }
@@ -81,12 +104,22 @@ public class ScoreManager : MonoBehaviour
         inPlay = true;
     }
 
-    // Reset the score
+    // Reset the only the set points
     public void ResetScore()
     {
-        // Set the scores to 0, right to serve, and the ball is in play
+        // Set the scores to 0 and the ball is in play
         side1Score = 0;
         side2Score = 0;
+        inPlay = true;
+    }
+    //Reset the entire Match
+    void ResetMatch()
+    {
+        // Set the scores to 0, the sets to 0, right to serve, and the ball is in play
+        side1Score = 0;
+        side2Score = 0;
+        side1SetsWon = 0;
+        side2SetsWon = 0;
         leftLastScored = false;
         inPlay = true;
     }
